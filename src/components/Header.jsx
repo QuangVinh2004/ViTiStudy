e3d65d431afc5a5e2936de3a2ab6392766cea829
Quang Vinh <trinhquangvinh07062004@gmail.com>
2025-07-10 08:00:28 +0700

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+
 
 function Header() {
     return (
@@ -13,66 +13,35 @@ function Header() {
                     />
                 </div>
 
-
-                {/* Search icon + Sign in */}
-                <div className="flex items-center space-x-4">
-                    <input
-                        type="text"
-                        placeholder=""
-                        className=" w-[230px] font-bold text-xl text-center text-gray-500 placeholder-gray-400  border-[1px] border-black rounded-full py-2 focus:outline-none focus:ring-2 focus:ring-black"
-                    />
-                    <div className="w-10 h-10 flex justify-center bg-[#eee] p-2 rounded-full">
+                {/* Search form */}
+                <div className="flex items-center space-x-4  border-[1px] border-black rounded-full  w-[400px] bg-white">
+                    <div className="w-10 h-10 flex justify-center  p-2 ">
                         <i className="fa-solid fa-magnifying-glass text-lg text-black"></i>
                     </div>
+                    <input
+                        type="text"
+                        placeholder="Tìm kiếm khóa học ,giáo viên,bài quizz,..."
+                        className=" w-[300px]   text-center text-gray-500 placeholder-gray-400  focus:outline-none focus:ring-2 focus:ring-black"
+                    />
+                    
+
+                </div>
+
+                {/*  Sign in + Sign up */}
+                <div>
+                    <button className="  text-black font-semibold px-6 py-2 ">
+                        Sign up
+                    </button>
                     <button className="bg-lime-300 border-2 border-black text-black font-semibold px-6 py-2 rounded-full hover:bg-lime-400 transition-all">
                         Sign in
                     </button>
                 </div>
+
             </div>
+
             {/* Divider line */}
-             <div className="w-full border-t border-gray-300 mb-4"></div>
-            {/* Navigation links */}
-            <div className=" py-2">
-                <nav className="flex justify-center space-x-32">
-                    <Link to="/" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-house"></i>
-                        <label>Home</label>
+            <div className="w-full border-t border-gray-300 mb-4"></div>
 
-                    </Link>
-                    <Link to="/about" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-info-circle"></i>
-                        <label>About</label>
-                        
-                    </Link>
-                    <Link to="/contact" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-envelope"></i>
-                        <label>Contact</label>
-                        
-                    </Link>
-                    <Link to="/" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-house"></i>
-                        <label>Home</label>
-
-                    </Link>
-                    <Link to="/about" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-info-circle"></i>
-                        <label>About</label>
-                        
-                    </Link>
-                    <Link to="/contact" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-envelope"></i>
-                        <label>Contact</label>
-                        
-                    </Link>
-                    <Link to="/" className="text-gray-600 hover:text-black flex flex-col items-center">
-                        <i className="fa-solid fa-house"></i>
-                        <label>Home</label>
-
-                    </Link>
-                   
-                </nav>
-            </div>
-         
         </header>
 
     );
