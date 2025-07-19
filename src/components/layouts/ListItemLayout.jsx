4300e7b60c68b5998ef0435ffddc090784b12678
Quang Vinh <trinhquangvinh07062004@gmail.com>
2025-07-19 17:53:06 +0700

    upadate ListItemLayout

--- src/components/layouts/ListItemLayout.jsx
+++ src/components/layouts/ListItemLayout.jsx
@@ -5,10 +5,10 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const ListItemLayout = ({ children, className = "" }) => {
   const scrollRef = useRef(null);
+  const cardCount = React.Children.count(children);
 
-  // 👉 Scroll 2 card mỗi lần (giả định mỗi card rộng 220px + 20px gap)
   const scrollByCards = (direction = "next") => {
-    const cardWidth = 350; // chỉnh nếu card bạn khác
+    const cardWidth = 350;
     const scrollDistance = cardWidth * 2;
 
     if (scrollRef.current) {
@@ -21,32 +21,34 @@ const ListItemLayout = ({ children, className = "" }) => {
 
   return (
     <div className="relative">
-      {/* Button Previous */}
-      <ButtonComponent
-        text=""
-        onClick={() => scrollByCards("prev")}
-        rightIcon={<FontAwesomeIcon icon={faArrowLeft} />}
-        className="absolute top-[40%] left-[-2%] -translate-y-1/2 z-10 w-[50px] h-[50px] rounded-full flex items-center justify-center shadow-md"
-        bgColor="bg-white"
-      />
-      {/* Scroll container */}
+      {cardCount >= 3 && (
+        <ButtonComponent
+          text=""
+          onClick={() => scrollByCards("prev")}
+          rightIcon={<FontAwesomeIcon icon={faArrowLeft} />}
+          className="absolute top-[40%] left-[-2%] -translate-y-1/2 z-10 w-[50px] h-[50px] rounded-full flex items-center justify-center shadow-md"
+          bgColor="bg-white"
+        />
+      )}
+
       <ScrollContainer
         innerRef={scrollRef}
-        className={`flex gap-5 p-2 overflow-x-auto cursor-grab ${className}`}
+        className={`flex gap-5 p-2 overflow-x-auto max-w-full cursor-grab ${className}`}
         vertical={false}
-        hideScrollbars={true} // ẩn luôn thanh trượt
+        hideScrollbars={true}
       >
         {React.Children.toArray(children)}
       </ScrollContainer>
 
-      {/* Button Next */}
-      <ButtonComponent
-        text=""
-        onClick={() => scrollByCards("next")}
-        rightIcon={<FontAwesomeIcon icon={faArrowRight} />}
-        className="absolute top-[40%] right-[-2%] -translate-y-1/2 z-10 w-[50px] h-[50px] rounded-full flex items-center justify-center shadow-md"
-        bgColor="bg-white"
-      />
+      {cardCount >= 3 && (
+        <ButtonComponent
+          text=""
+          onClick={() => scrollByCards("next")}
+          rightIcon={<FontAwesomeIcon icon={faArrowRight} />}
+          className="absolute top-[40%] right-[-2%] -translate-y-1/2 z-10 w-[50px] h-[50px] rounded-full flex items-center justify-center shadow-md"
+          bgColor="bg-white"
+        />
+      )}
     </div>
   );
 };
